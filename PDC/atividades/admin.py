0f56fa4300d6565e558c5b6aab23836899b15bb7
Keep Simple <keepsimple17@gmail.com>
2017-09-21 18:54:03 -0300

    Adicionado modelo estático de página para Atividades & Eventos

--- PDC/atividades/admin.py
+++ PDC/atividades/admin.py
@@ -5,5 +5,6 @@ from .models import Evento
 class EventoAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'origem', 'created_at']
     search_fields = ['title','mensagem']
+    prepopulated_fields = {'slug':('title',)}
 
 admin.site.register(Evento,EventoAdmin)
--- PDC/atividades/models.py
+++ PDC/atividades/models.py
@@ -31,6 +31,10 @@ class Evento(models.Model):
     def __str__(self):
         return self.title
 
+    @models.permalink
+    def get_absolute_url(self):
+        return ('atividades:details',(),{'slug':self.slug})
+
     class Meta:
         verbose_name = "Atividade"
         verbose_name_plural = "Eventos"
