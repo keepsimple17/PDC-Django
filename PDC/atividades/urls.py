8a311ec1233a00d3c5f510f13c220f0204bc4fe0
Keep Simple <keepsimple17@gmail.com>
2018-03-11 11:14:03 +0800

    remove former files

--- PDC/atividades/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from PDC.atividades import views as AtividadesViews
-
-urlpatterns = [
-    url(r'^$', AtividadesViews.index, name="index"),
-    url(r'^(?P<pk>\d+)/$', AtividadesViews.detalhes , name='detalhes'),
-    #url(r'^(?P<slug>[\w_-]+)/$', AtividadesViews.detalhes, name='detalhes'),
-]
