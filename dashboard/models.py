f33451b0835a098336ef01ae252b1bb337b43425
Keep Simple <keepsimple17@gmail.com>
2018-03-14 23:46:14 -0300

    adding regions, cities and states to the generic database

--- dashboard/models.py
+++ dashboard/models.py
@@ -12,9 +12,90 @@ POLITICAL_PARTY_CHOICES = (
     ('beer', 'Beer'),
 )
 
+#Political Spectrum is the ideological posture of the Political Party (ex. democrats or republicans | left wing or right wing)
+PoliticalSpectrum = (
+    ('Centro', 'Centro'),
+    ('CentroDireita','Centro Direita'),
+    ('CentroEsquerda', 'Centro Esquerda'),
+    ('Esquerda', 'Esquerda'),
+    ('ExtremaDireita', 'Extrema Direita'),
+    ('ExtremaEsquerda', 'Extrema Esquerda'),
+)
+
+#Regions In Brazil (used to define the State location (and Citie location)
+Regioes = (
+    (1, 'Norte'),
+    (2, 'Nordeste'),
+    (3, 'Sudeste'),
+    (4,'Sul'),
+    (5, 'Centro-Oeste')
+)
+
+# Manager for the Federal States in Brazil
+class EstadoManager(models.Manager):
+    def search(self,query):
+        return self.get_queryset().filter(
+            models.Q(Nome__icontains=query) | models.Q(Uf__icontains=query)
+        )
+
+# This is the Manager for the Cities (Conties) in Brazil
+class MunicipioManager(models.Manager):
+    def search(self,query):
+        return self.get_queryset().filter(
+            models.Q(Nome__icontains=query) | models.Q(Uf__icontains=query)
+        )
+
+# This is the Manager for the Candidate Political Party
+class PoliticalPartiesManager(models.Manager):
+    def search(self,query):
+        return self.get_queryset().filter(
+            models.Q(electoral_number__icontains=query) | models.Q(sigla__icontains=query)
+        )
+
+
+# Political Partie list
+class PoliticalParties(models.Model):
+    id_partido = models.IntegerField("Numero Partido",primary_key=True, unique=True)
+    sigla = models.CharField('Sigla', max_length=6) # party symbol
+    name = models.CharField('Nome do Partido', max_length=70) # party complete name
+    electoral_number = models.CharField("Número Eleitoral", max_length=3) #the number that indicate the Political Party
+    affiliates_number = models.IntegerField("Número de Filiados")
+    president_name = models.CharField("Presidente Atual", max_length=60) #The present political leader of the Political Party
+    political_spectrum = models.CharField(max_length=100, choices=PoliticalSpectrum, blank=True, null=True)
+    image_flag = models.ImageField(
+        upload_to='images/political_flags', verbose_name='bandeiraPartido', null=True, blank=True
+    )
+    objects = PoliticalPartiesManager()
+
+
+# Table of Federal States in Brazil
+class Estado (models.Model):
+    Id = models.IntegerField(primary_key=True,unique=True)
+    CodigoUf = models.PositiveSmallIntegerField(unique=True)
+    Nome=models.CharField(max_length=50)
+    Uf=models.CharField(max_length=2,unique=True)
+    Regiao=models.PositiveSmallIntegerField(blank=False)
+    objects = EstadoManager
+
+
+# This is the conties database. All cities in Brasil
+class Municipio(models.Model):
+    Id = models.IntegerField(primary_key=True, unique=True)
+    Codigo = models.IntegerField(null=False, unique=True) # unique Federal Code to the Region
+    Nome = models.CharField(max_length=255)
+    Uf = models.CharField(max_length=2)
+    objects=MunicipioManager()
+
+# This is the Neiborhood locations, for the address forms and data location
+class Bairro(models.Model):
+    Id = models.IntegerField(primary_key=True,unique=True)
+    Codigo = models.CharField(max_length=10, unique=True)
+    Nome = models.CharField(max_length=255,null=False)
+    Uf = models.CharField(max_length=2)
+
+
 class Profile(models.Model):
     # This will represent an user account profile entity
-
     user = models.OneToOneField(User, null=False, blank=False)
     location = models.CharField(max_length=50, blank=True, null=True)
     political_party = models.CharField(max_length=50, choices=POLITICAL_PARTY_CHOICES, blank=True, null=True)
