20d3db6a3eb74636bb4bca1e0d4aae258ac6d253
Keep Simple <keepsimple17@gmail.com>
2018-03-17 19:14:10 -0300

    adding the app candidates

---
+++ candidato/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CandidatoConfig(AppConfig):
+    name = 'candidato'
--- core/settings.py
+++ core/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'pdc_db',
+        'NAME': 'pdc',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
