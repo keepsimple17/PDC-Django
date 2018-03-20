5886c8a74ea14db462e96a6fe742684cd3948320
Keep Simple <keepsimple17@gmail.com>
2018-03-21 03:44:13 +0800

    modify const

--- core/views.py
+++ core/views.py
@@ -38,7 +38,7 @@ def signup(request):
             user.is_active = False
             user.save()
             current_site = get_current_site(request)
-            mail_subject = 'Activate your blog account.'
+            mail_subject = 'Activate your PDC account.'
             message = render_to_string('acc_active_email.html', {
                 'user': user,
                 'domain': current_site.domain,
