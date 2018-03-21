84fa1b1f1bd6cd0c5ecd4a44bf036c1aabad12d7
Keep Simple <keepsimple17@gmail.com>
2018-03-22 01:21:29 +0800

    feat

--- core/settings.py
+++ core/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'pdc',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
         # 'ENGINE': 'django.db.backends.mysql',
