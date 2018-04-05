8fa79cd94dbc74fe6c45531dd3042ef5804843f5
Keep Simple <keepsimple17@gmail.com>
2018-04-05 13:15:02 -0300

    creating the new screens on new_design

--- core/settings.py
+++ core/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pdc',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
         # 'ENGINE': 'django.db.backends.mysql',
