4137a03694e46968751eba70c3bdf0f19b2429dd
Keep Simple <keepsimple17@gmail.com>
2018-06-13 11:13:06 -0300

    new changes on design ui

--- dashboard/views.py
+++ dashboard/views.py
@@ -14,14 +14,18 @@ from django.utils.translation import ugettext_lazy as _
 
 # Create your views here.
 
+candidato = {'candidato': "Beto Richa", 'candidato_UF': 'PR',
+             'candidato_city': 'Curitiba', 'candidato_intended_position': "Senador",
+             'ballot': "Eleições 2018"
+             }
 
 @login_required
 def index(request):
     # excel_list = []
     # print(request.user)
-    return render(request, 'index.html', {})
+    return render(request, 'index.html', candidato)
 
 
 @login_required
 def stats(request):
-    return render(request, 'stats/resumo.html')
+    return render(request, 'stats/resumo.html', candidato)
