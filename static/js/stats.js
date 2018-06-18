dc126f132a871d7cb6e1625850ab336932a5389d
Keep Simple <keepsimple17@gmail.com>
2018-06-18 23:42:19 +0800

    feat(resumo): add pie chart

--- static/js/stats.js
+++ static/js/stats.js
@@ -366,14 +366,15 @@ $(function () {
       'echarts',
       'echarts/theme/limitless',
       'echarts/chart/bar',
-      'echarts/chart/line'
+      'echarts/chart/line',
+      'echarts/chart/pie'
     ],
 
     // Charts setup
     function (ec, limitless) {
 
       axios.get('/elections/dispute/', {
-        params: {},
+        params: {}
       })
         .then((res) => {
           console.log('dispute', res.data);
@@ -383,6 +384,7 @@ $(function () {
         });
 
       const basic_bars = ec.init(document.getElementById('basic_bars'), limitless);
+      const pie_timeline = ec.init(document.getElementById('pie_timeline'), limitless);
 
       const basic_bars_options = {
 
@@ -447,13 +449,270 @@ $(function () {
         ]
       };
 
+      var idx = 1;
+      pie_timeline_options = {
+
+        // Add timeline
+        timeline: {
+          x: 10,
+          x2: 10,
+          data: [
+            '2014-01-01', '2014-02-01', '2014-03-01', '2014-04-01', '2014-05-01',
+            {name: '2014-06-01', symbol: 'emptyStar2', symbolSize: 8},
+            '2014-07-01', '2014-08-01', '2014-09-01', '2014-10-01', '2014-11-01',
+            {name: '2014-12-01', symbol: 'star2', symbolSize: 8}
+          ],
+          label: {
+            formatter: function (s) {
+              return s.slice(0, 7);
+            }
+          },
+          autoPlay: true,
+          playInterval: 3000
+        },
+
+        // Set options
+        options: [
+          {
+
+            // Add title
+            title: {
+              text: 'Browser statistics',
+              subtext: 'Based on shared research',
+              x: 'center'
+            },
+
+            // Add tooltip
+            tooltip: {
+              trigger: 'item',
+              formatter: "{a} <br/>{b}: {c} ({d}%)"
+            },
+
+            // Add legend
+            legend: {
+              x: 'left',
+              orient: 'vertical',
+              data: ['Chrome', 'Firefox', 'Safari', 'IE9+', 'IE8-']
+            },
+
+            // Display toolbox
+            toolbox: {
+              show: true,
+              orient: 'vertical',
+              feature: {
+                mark: {
+                  show: true,
+                  title: {
+                    mark: 'Markline switch',
+                    markUndo: 'Undo markline',
+                    markClear: 'Clear markline'
+                  }
+                },
+                dataView: {
+                  show: true,
+                  readOnly: false,
+                  title: 'View data',
+                  lang: ['View chart data', 'Close', 'Update']
+                },
+                magicType: {
+                  show: true,
+                  title: {
+                    pie: 'Switch to pies',
+                    funnel: 'Switch to funnel'
+                  },
+                  type: ['pie', 'funnel'],
+                  option: {
+                    funnel: {
+                      x: '25%',
+                      width: '50%',
+                      funnelAlign: 'left',
+                      max: 1700
+                    }
+                  }
+                },
+                restore: {
+                  show: true,
+                  title: 'Restore'
+                },
+                saveAsImage: {
+                  show: true,
+                  title: 'Same as image',
+                  lang: ['Save']
+                }
+              }
+            },
+
+            // Add series
+            series: [{
+              name: 'Browser',
+              type: 'pie',
+              center: ['50%', '50%'],
+              radius: '60%',
+              data: [
+                {value: idx * 128 + 80, name: 'Chrome'},
+                {value: idx * 64 + 160, name: 'Firefox'},
+                {value: idx * 32 + 320, name: 'Safari'},
+                {value: idx * 16 + 640, name: 'IE9+'},
+                {value: idx++ * 8 + 1280, name: 'IE8-'}
+              ]
+            }]
+          },
+
+          {
+            series: [{
+              name: 'Browser',
+              type: 'pie',
+              data: [
+                {value: idx * 128 + 80, name: 'Chrome'},
+                {value: idx * 64 + 160, name: 'Firefox'},
+                {value: idx * 32 + 320, name: 'Safari'},
+                {value: idx * 16 + 640, name: 'IE9+'},
+                {value: idx++ * 8 + 1280, name: 'IE8-'}
+              ]
+            }]
+          },
+          {
+            series: [{
+              name: 'Browser',
+              type: 'pie',
+              data: [
+                {value: idx * 128 + 80, name: 'Chrome'},
+                {value: idx * 64 + 160, name: 'Firefox'},
+                {value: idx * 32 + 320, name: 'Safari'},
+                {value: idx * 16 + 640, name: 'IE9+'},
+                {value: idx++ * 8 + 1280, name: 'IE8-'}
+              ]
+            }]
+          },
+          {
+            series: [{
+              name: 'Browser',
+              type: 'pie',
+              data: [
+                {value: idx * 128 + 80, name: 'Chrome'},
+                {value: idx * 64 + 160, name: 'Firefox'},
+                {value: idx * 32 + 320, name: 'Safari'},
+                {value: idx * 16 + 640, name: 'IE9+'},
+                {value: idx++ * 8 + 1280, name: 'IE8-'}
+              ]
+            }]
+          },
+          {
+            series: [{
+              name: 'Browser',
+              type: 'pie',
+              data: [
+                {value: idx * 128 + 80, name: 'Chrome'},
+                {value: idx * 64 + 160, name: 'Firefox'},
+                {value: idx * 32 + 320, name: 'Safari'},
+                {value: idx * 16 + 640, name: 'IE9+'},
+                {value: idx++ * 8 + 1280, name: 'IE8-'}
+              ]
+            }]
+          },
+          {
+            series: [{
+              name: 'Browser',
+              type: 'pie',
+              data: [
+                {value: idx * 128 + 80, name: 'Chrome'},
+                {value: idx * 64 + 160, name: 'Firefox'},
+                {value: idx * 32 + 320, name: 'Safari'},
+                {value: idx * 16 + 640, name: 'IE9+'},
+                {value: idx++ * 8 + 1280, name: 'IE8-'}
+              ]
+            }]
+          },
+          {
+            series: [{
+              name: 'Browser',
+              type: 'pie',
+              data: [
+                {value: idx * 128 + 80, name: 'Chrome'},
+                {value: idx * 64 + 160, name: 'Firefox'},
+                {value: idx * 32 + 320, name: 'Safari'},
+                {value: idx * 16 + 640, name: 'IE9+'},
+                {value: idx++ * 8 + 1280, name: 'IE8-'}
+              ]
+            }]
+          },
+          {
+            series: [{
+              name: 'Browser',
+              type: 'pie',
+              data: [
+                {value: idx * 128 + 80, name: 'Chrome'},
+                {value: idx * 64 + 160, name: 'Firefox'},
+                {value: idx * 32 + 320, name: 'Safari'},
+                {value: idx * 16 + 640, name: 'IE9+'},
+                {value: idx++ * 8 + 1280, name: 'IE8-'}
+              ]
+            }]
+          },
+          {
+            series: [{
+              name: 'Browser',
+              type: 'pie',
+              data: [
+                {value: idx * 128 + 80, name: 'Chrome'},
+                {value: idx * 64 + 160, name: 'Firefox'},
+                {value: idx * 32 + 320, name: 'Safari'},
+                {value: idx * 16 + 640, name: 'IE9+'},
+                {value: idx++ * 8 + 1280, name: 'IE8-'}
+              ]
+            }]
+          },
+          {
+            series: [{
+              name: 'Browser',
+              type: 'pie',
+              data: [
+                {value: idx * 128 + 80, name: 'Chrome'},
+                {value: idx * 64 + 160, name: 'Firefox'},
+                {value: idx * 32 + 320, name: 'Safari'},
+                {value: idx * 16 + 640, name: 'IE9+'},
+                {value: idx++ * 8 + 1280, name: 'IE8-'}
+              ]
+            }]
+          },
+          {
+            series: [{
+              name: 'Browser',
+              type: 'pie',
+              data: [
+                {value: idx * 128 + 80, name: 'Chrome'},
+                {value: idx * 64 + 160, name: 'Firefox'},
+                {value: idx * 32 + 320, name: 'Safari'},
+                {value: idx * 16 + 640, name: 'IE9+'},
+                {value: idx++ * 8 + 1280, name: 'IE8-'}
+              ]
+            }]
+          },
+          {
+            series: [{
+              name: 'Browser',
+              type: 'pie',
+              data: [
+                {value: idx * 128 + 80, name: 'Chrome'},
+                {value: idx * 64 + 160, name: 'Firefox'},
+                {value: idx * 32 + 320, name: 'Safari'},
+                {value: idx * 16 + 640, name: 'IE9+'},
+                {value: idx++ * 8 + 1280, name: 'IE8-'}
+              ]
+            }]
+          }
+        ]
+      };
+
       basic_bars.setOption(basic_bars_options);
+      pie_timeline.setOption(pie_timeline_options);
       // Resize charts
       // ------------------------------
 
       window.onresize = function () {
         setTimeout(function () {
           basic_bars.resize();
+          pie_timeline.resize();
         }, 200);
       };
     }
@@ -493,3 +752,5 @@ $(function () {
     }
   });
 });
+
+
