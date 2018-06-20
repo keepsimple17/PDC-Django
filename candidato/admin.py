020a9e36889d630368056a40a590ad4fa8367067
Keep Simple <keepsimple17@gmail.com>
2018-06-20 23:57:35 +0800

    feat

--- candidato/admin.py
+++ candidato/admin.py
@@ -1,7 +1,60 @@
 from django.contrib import admin
-from django.apps import apps
 
-app = apps.get_app_config('candidato')
+from candidato.models import (
+    Candidate, Coalitions, Committees, CommitteeMembers, UserRoles, UserRolesList, ScopeList, Proposal, Keyword)
 
-for model_name, model in app.models.items():
-    admin.site.register(model)
+
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'user', 'candidate_political_nickname', 'created_at', 'updated_at')
+
+
+class CoalitionsAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name_coligation', 'political_partie_number', 'political_partie_abbreviation')
+
+
+class CommitteesAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'candidate', 'responsible')
+
+
+class CommitteeMembersAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'commitee', 'usuario', 'assignments', 'observations')
+
+
+class UserRolesAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'candidate', 'user', 'role_name')
+
+
+class UserRolesListAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'role_name', 'budget_managment', 'members_managment', 'reports_managment', 'members_access')
+
+
+class ScopeListAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'is_template', 'created_at', 'updated_at', 'user')
+
+
+class ProposalAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'scope', 'created_at', 'updated_at')
+
+
+class KeywordAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'keyword', 'type', 'created_at', 'updated_at')
+
+
+admin.site.register(Candidate, CandidateAdmin)
+admin.site.register(Coalitions, CoalitionsAdmin)
+admin.site.register(Committees, CommitteesAdmin)
+admin.site.register(CommitteeMembers, CommitteeMembersAdmin)
+admin.site.register(UserRoles, UserRolesAdmin)
+admin.site.register(UserRolesList, UserRolesListAdmin)
+admin.site.register(ScopeList, ScopeListAdmin)
+admin.site.register(Proposal, ProposalAdmin)
+admin.site.register(Keyword, KeywordAdmin)
--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,7 +1,40 @@
 from django.contrib import admin
-from django.apps import apps
 
-app = apps.get_app_config('dashboard')
+from dashboard.models import (Usuario, PoliticalParties, Estado, Municipio, Bairro, RelationshipNetwork)
 
-for model_name, model in app.models.items():
-    admin.site.register(model)
+
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'user', 'gender', 'address', 'cellPhone', 'landlinePhone', 'cep', 'estado', 'cidade', 'bairro',)
+
+
+class PoliticalPartiesAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'sigla', 'name', 'president_name')
+
+
+class EstadoAdmin(admin.ModelAdmin):
+    list_display = ('CodigoUf', 'Nome', 'Uf', 'Regiao')
+
+
+class MunicipioAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'Codigo', 'Nome', 'Uf')
+
+
+class BairroAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'Codigo', 'Nome', 'Uf')
+
+
+class RelationshipNetworkAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'id_actor_a', 'kind_of_relationship', 'id_actor_b')
+
+
+admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(PoliticalParties, PoliticalPartiesAdmin)
+admin.site.register(Estado, EstadoAdmin)
+admin.site.register(Municipio, MunicipioAdmin)
+admin.site.register(Bairro, BairroAdmin)
+admin.site.register(RelationshipNetwork, RelationshipNetworkAdmin)
