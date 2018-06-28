c171f8a7596de15954658f2ee5c0645c687f2a63
Keep Simple <keepsimple17@gmail.com>
2018-06-29 03:47:32 +0800

    feat(admin site)

--- candidato/admin.py
+++ candidato/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django import forms
 
 from candidato.models import (
     Candidate, Coalitions, Committees, CommitteeMembers, UserRoles, UserRolesList, ScopeList, Proposal, Keyword)
