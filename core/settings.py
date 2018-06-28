adf43a382f37bae8aaa56df7fad68d33d28f245b
Keep Simple <keepsimple17@gmail.com>
2018-06-28 09:44:21 +0800

    feat(adminsite)

--- core/settings.py
+++ core/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 
     'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
