bb7db6a64b3fa69040f18de26b96b2d7d8937dee
Keep Simple <keepsimple17@gmail.com>
2018-07-09 23:06:09 +0800

    fix(stats): js runtime error

--- static/js/stats.js
+++ static/js/stats.js
@@ -22,85 +22,6 @@ $(function () {
     $('#candidate_state_campaign').html(candidator.state_campaign || 'PR');
   };
 
-  // solid gauge start
-  const names = ['Bolsonaro', 'Ciro Gomes', 'Alckmin', 'Marina Silva', 'Joaquim Barbosa', 'Collor', 'Paulino'];
-  const data = [22, 15, 6, 5, 7, 10, 65];
-  const dataSet = anychart.data.set(data);
-  const palette = anychart.palettes.distinctColors().items(['#64b5f6', '#1976d2', '#ef6c00', '#ffd54f', '#455a64', '#96a6a6', '#dd2c00', '#00838f', '#00bfa5', '#ffa000']);
-
-  const makeBarWithBar = function (gauge, radius, i, width, without_stroke) {
-    let stroke = '1 #e5e4e4';
-    if (without_stroke) {
-      stroke = null;
-      gauge.label(i)
-        .text(names[i] + ', <span style="">' + data[i] + '%</span>')// color: #7c868e
-        .useHtml(true);
-      gauge.label(i)
-        .hAlign('center')
-        .vAlign('middle')
-        .anchor('right-center')
-        .padding(0, 10)
-        .height(width / 2 + '%')
-        .offsetY(radius + '%')
-        .offsetX(0);
-    }
-
-    gauge.bar(i).dataIndex(i)
-      .radius(radius)
-      .width(width)
-      .fill(palette.itemAt(i))
-      .stroke(null)
-      .zIndex(5);
-    gauge.bar(i + 100).dataIndex(5)
-      .radius(radius)
-      .width(width)
-      .fill('#F5F4F4')
-      .stroke(stroke)
-      .zIndex(4);
-
-    return gauge.bar(i);
-  };
-
-  anychart.onDocumentReady(function () {
-    const gauge = anychart.gauges.circular();
-    gauge.data(dataSet);
-    gauge.fill('#fff')
-      .stroke(null)
-      .padding(0)
-      .margin(100)
-      .startAngle(0)
-      .sweepAngle(270);
-
-    const axis = gauge.axis().radius(100).width(1).fill(null);
-    axis.scale()
-      .minimum(0)
-      .maximum(100)
-      .ticks({interval: 1})
-      .minorTicks({interval: 1});
-
-    axis.labels().enabled(false);
-    axis.ticks().enabled(false);
-    axis.minorTicks().enabled(false);
-    makeBarWithBar(gauge, 100, 0, 17, true);
-    makeBarWithBar(gauge, 80, 1, 17, true);
-    makeBarWithBar(gauge, 60, 2, 17, true);
-    makeBarWithBar(gauge, 40, 3, 17, true);
-    makeBarWithBar(gauge, 20, 4, 17, true);
-
-    gauge.margin(50);
-    gauge.title().text('Lideranças em Pesquisas de Voto' +
-      '<br/><span style="color:#929292; font-size: 12px;">(Densidade de Citações - 94.6% precisão       )</span>').useHtml(true);
-    gauge.title()
-      .enabled(true)
-      .hAlign('center')
-      .padding(0)
-      .margin([0, 0, 20, 0]);
-
-    gauge.container('gauge_container');
-    gauge.draw();
-  });
-  // solid gauge end
-
   // brazil map start
   anychart.onDocumentReady(function () {
     const dataSet = [
@@ -229,6 +150,86 @@ $(function () {
     map.draw();
   });
   // brazil map end
+
+  // solid gauge start
+  const names = ['Bolsonaro', 'Ciro Gomes', 'Alckmin', 'Marina Silva', 'Joaquim Barbosa', 'Collor', 'Paulino'];
+  const data = [22, 15, 6, 5, 7, 10, 65];
+  const dataSet = anychart.data.set(data);
+  const palette = anychart.palettes.distinctColors().items(['#64b5f6', '#1976d2', '#ef6c00', '#ffd54f', '#455a64', '#96a6a6', '#dd2c00', '#00838f', '#00bfa5', '#ffa000']);
+
+  const makeBarWithBar = function (gauge, radius, i, width, without_stroke) {
+    let stroke = '1 #e5e4e4';
+    if (without_stroke) {
+      stroke = null;
+      gauge.label(i)
+        .text(names[i] + ', <span style="">' + data[i] + '%</span>')// color: #7c868e
+        .useHtml(true);
+      gauge.label(i)
+        .hAlign('center')
+        .vAlign('middle')
+        .anchor('right-center')
+        .padding(0, 10)
+        .height(width / 2 + '%')
+        .offsetY(radius + '%')
+        .offsetX(0);
+    }
+
+    gauge.bar(i).dataIndex(i)
+      .radius(radius)
+      .width(width)
+      .fill(palette.itemAt(i))
+      .stroke(null)
+      .zIndex(5);
+    gauge.bar(i + 100).dataIndex(5)
+      .radius(radius)
+      .width(width)
+      .fill('#F5F4F4')
+      .stroke(stroke)
+      .zIndex(4);
+
+    return gauge.bar(i);
+  };
+
+  anychart.onDocumentReady(function () {
+    const gauge = anychart.gauges.circular();
+    gauge.data(dataSet);
+    gauge.fill('#fff')
+      .stroke(null)
+      .padding(0)
+      .margin(100)
+      .startAngle(0)
+      .sweepAngle(270);
+
+    const axis = gauge.axis().radius(100).width(1).fill(null);
+    axis.scale()
+      .minimum(0)
+      .maximum(100)
+      .ticks({interval: 1})
+      .minorTicks({interval: 1});
+
+    axis.labels().enabled(false);
+    axis.ticks().enabled(false);
+    axis.minorTicks().enabled(false);
+    makeBarWithBar(gauge, 100, 0, 17, true);
+    makeBarWithBar(gauge, 80, 1, 17, true);
+    makeBarWithBar(gauge, 60, 2, 17, true);
+    makeBarWithBar(gauge, 40, 3, 17, true);
+    makeBarWithBar(gauge, 20, 4, 17, true);
+
+    gauge.margin(50);
+    gauge.title().text('Lideranças em Pesquisas de Voto' +
+      '<br/><span style="color:#929292; font-size: 12px;">(Densidade de Citações - 94.6% precisão       )</span>').useHtml(true);
+    gauge.title()
+      .enabled(true)
+      .hAlign('center')
+      .padding(0)
+      .margin([0, 0, 20, 0]);
+
+    gauge.container('gauge_container');
+    gauge.draw();
+  });
+  // solid gauge end
+
   /* tree chart start */
   anychart.onDocumentReady(function () {
 
