f38ef0611014dab36a437a17b5153a17d3ade619
Keep Simple <keepsimple17@gmail.com>
2018-07-10 11:51:17 +0800

    feat(stats): add pie chart

--- static/js/stats.js
+++ static/js/stats.js
@@ -379,7 +379,7 @@ $(function () {
   for (const item of candidates) {
     nick_name_list.push(item.candidate_political_nickname);
   }
-  console.log('nick_name_list', nick_name_list);
+
   /* bar chart */
   require.config({
     paths: {
@@ -411,6 +411,7 @@ $(function () {
 
       const basic_bars = ec.init(document.getElementById('basic_bars'), limitless);
       const pie_timeline = ec.init(document.getElementById('pie_timeline'), limitless);
+      const pie_basic_element = ec.init(document.getElementById('pie_basic'), limitless);
 
       const basic_bars_options = {
 
@@ -734,8 +735,82 @@ $(function () {
         ]
       };
 
+      const pie_basic_options = {
+        // Colors
+        color: [
+          '#2ec7c9','#b6a2de','#5ab1ef','#ffb980','#d87a80',
+          '#8d98b3','#e5cf0d','#97b552','#95706d','#dc69aa',
+          '#07a2a4','#9a7fd1','#588dd5','#f5994e','#c05050',
+          '#59678c','#c9ab00','#7eb00a','#6f5553','#c14089'
+        ],
+
+        // Global text styles
+        textStyle: {
+          fontFamily: 'Roboto, Arial, Verdana, sans-serif',
+          fontSize: 13
+        },
+
+        // Add title
+        title: {
+          text: 'Browser popularity',
+          subtext: 'Open source information',
+          left: 'center',
+          textStyle: {
+              fontSize: 17,
+              fontWeight: 500
+          },
+          subtextStyle: {
+              fontSize: 12
+          }
+        },
+
+        // Add tooltip
+        tooltip: {
+          trigger: 'item',
+          backgroundColor: 'rgba(0,0,0,0.75)',
+          padding: [10, 15],
+          textStyle: {
+              fontSize: 13,
+              fontFamily: 'Roboto, sans-serif'
+          },
+          formatter: "{a} <br/>{b}: {c} ({d}%)"
+        },
+
+        // Add legend
+        legend: {
+          orient: 'vertical',
+          top: 'center',
+          left: 0,
+          data: ['IE', 'Opera', 'Safari', 'Firefox', 'Chrome'],
+          itemHeight: 8,
+          itemWidth: 8
+        },
+
+        // Add series
+        series: [{
+          name: 'Browsers',
+          type: 'pie',
+          radius: '70%',
+          center: ['50%', '57.5%'],
+          itemStyle: {
+            normal: {
+              borderWidth: 1,
+              borderColor: '#fff'
+            }
+          },
+          data: [
+            {value: 335, name: 'IE'},
+            {value: 310, name: 'Opera'},
+            {value: 234, name: 'Safari'},
+            {value: 135, name: 'Firefox'},
+            {value: 1548, name: 'Chrome'}
+          ]
+        }]
+      };
+
       basic_bars.setOption(basic_bars_options);
       pie_timeline.setOption(pie_timeline_options);
+      pie_basic_element.setOption(pie_basic_options);
       // Resize charts
       // ------------------------------
 
@@ -743,6 +818,7 @@ $(function () {
         setTimeout(function () {
           basic_bars.resize();
           pie_timeline.resize();
+          pie_basic_element.resize();
         }, 200);
       };
     }
